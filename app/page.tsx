e2ec86649cc1134a8b1efc3d1bbc27ab76b07470
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-07-10 17:36:59 +0530

    intial schema for sonu tube

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+"use client"
+
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 export default function Home() {
   return (
-    <></>
+    <div className="h-screen w-full flex justify-center items-center" >
+      <ReactPlayer controls={true} playing={true} url={"https://res.cloudinary.com/dzwbh0yob/video/upload/v1720553636/pklhbokbcaxykrmbcnfp.mp4"}></ReactPlayer>
+    </div>
   );
 }
