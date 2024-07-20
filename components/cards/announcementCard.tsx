074a9505a9704a239e2bf244f2a209bb409ae585
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-07-21 01:41:19 +0530

    added  bottom nav

--- components/cards/announcementCard.tsx
+++ components/cards/announcementCard.tsx
@@ -7,7 +7,7 @@ import AnnouncementBookmark from "./annoucementBookmark";
 export default function AnnoucementCard(props:any){
     const token:any = cookies().get('token')
     const decoded:any = jwtDecode(token?.value)
-    return <div className="bg-white w-96 h-80 p-3 border-2 border-gray-100 shadow-sm flex flex-col justify-start items-start text-black rounded-lg ">
+    return <div className="bg-white w-[350px] h-80 p-3 border-2 border-gray-100 shadow-sm flex flex-col justify-start items-start text-black rounded-lg ">
         <div className="flex w-full justify-between items-center">
             <div className="flex justify-between items-center">
         <Avatar>
@@ -21,7 +21,7 @@ export default function AnnoucementCard(props:any){
         </div>
         <div className="pt-2">
         <p className="font-semibold text-lg">{props.title}</p>
-        <div className="overflow-ellipsis overflow-hidden  w-[370] h-44 pt-1">{props.discription}</div>
+        <div className="overflow-ellipsis overflow-hidden  w-[320] h-44 pt-1">{props.discription}</div>
         </div>
         <div className="flex w-full justify-between">
         <AnnoucementHandler userlike={props.userlike} userdislike={props.userdislike} id={props.id} likes={props.likes} dislikes={props.dislikes}></AnnoucementHandler>
