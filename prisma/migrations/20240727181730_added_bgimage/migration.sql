26d5b240a07d20fb9d0d6427c06e09f358bf2785
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-07-27 23:47:59 +0530

    migrate

---
+++ prisma/migrations/20240727181730_added_bgimage/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "bgurl" TEXT NOT NULL DEFAULT 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.inc.com%2Famy-george%2Fwhat-your-linkedin-cover-photo-says-about-you-if-you-have-one-you-better-have-one.html&psig=AOvVaw1XWIdYX1nU1nv3b3OYqh0B&ust=1722179838084000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMC4yf_Bx4cDFQAAAAAdAAAAABAE';
