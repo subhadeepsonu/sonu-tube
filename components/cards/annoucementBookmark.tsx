ea864262825e391cf5ba18f5081bf831ee8f25ff
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-02 02:23:42 +0530

    bookmark bug fixed

--- components/cards/annoucementBookmark.tsx
+++ components/cards/annoucementBookmark.tsx
@@ -4,6 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { FaRegBookmark } from "react-icons/fa6";
 import { FaBookmark } from "react-icons/fa6";
+import { toast } from "sonner";
 export default function AnnouncementBookmark(props:{
     userid:string,
     announcementid:number,
@@ -11,20 +12,26 @@ export default function AnnouncementBookmark(props:{
 }){
     const MutateAddBookmark = useMutation({
         mutationFn:()=>AddBookMark(props.userid,props.announcementid),
-        onSettled:(data)=>{
-            console.log(data)
+        onSuccess:()=>{
+            toast.success("Bookmark added")
+        },
+        onError:()=>{
+            toast.error("Something went wrong")
         }
     })
     const MutateRemoveBookmark  =  useMutation({
         mutationFn:()=>RemoveBookMark(props.userid,props.announcementid),
-        onSettled:(data)=>{
-            console.log(data)
+        onSuccess:()=>{
+            toast.warning("Bookmark removed")
+        },
+        onError:()=>{
+            toast.error("Something went wrong")
         }
     })
     const [bookmark,setBookmark] = useState<boolean>(false)
     useEffect(()=>{
         {props.bookmarks.forEach((bookmark)=>{
-            if(bookmark.userid=props.userid){
+            if(bookmark.userid==props.userid){
                 setBookmark(true)
             }
         })}
