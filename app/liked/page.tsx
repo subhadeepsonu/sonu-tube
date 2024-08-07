832ccfae2516ea5a7fbac646b3fd9ea4fd49d10c
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-07 13:49:03 +0530

    added video owner  id video card to redirect to profile

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -8,12 +8,12 @@ export default async function LikedPage(){
     const data = await GetLikedVideo(decoded.id)
     return <div className="min-h-screen w-full flex flex-col justify-start bg-gray-100 items-center pt-20 md:pl-20">
             <p className="text-2xl p-2 font-semibold">Liked Videos</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">                
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5">                
                 {data.map((video)=>{
-                    return <VideoCard videoholderid="" videourl={video.video.videourl} watchlater={video.video.watchlater} userid={video.video.userid} key={video.id} id={video.video.id} userimage={video.video.user.imgurl} name={video.video.user
+                    return <VideoCard videoholderid={video.userId} videourl={video.video.videourl} watchlater={video.video.watchlater} userid={decoded.id} key={video.id} id={video.video.id} userimage={video.video.user.imgurl} name={video.video.user
                         .name} views={video.video._count.views} imgurl={video.video.thumnailurl} title={video.video.title}
                     ></VideoCard>
                 })}
-            </div>
+            </div>  
     </div>      
 }
