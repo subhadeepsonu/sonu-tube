f9c2587b7592287b2285e425020767d9bc184641
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-11 11:32:50 +0530

    improved intractivity in video card by using timeouts

--- components/cards/hovervideo.tsx
+++ components/cards/hovervideo.tsx
@@ -1,20 +1,43 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useRef } from "react"
 import ReactPlayer from "react-player"
 
 export default function HoverVideo(props:{
     imgurl:string,
     videourl:string
 }){
-    const [check,Setcheck] = useState(false)
-    return <div onMouseEnter={()=>{
-        Setcheck(true)
-    }}
-    onMouseLeave={()=>{
-        Setcheck(false)
-    }}
-    className="w-80 h-56">
-        {(check)?<ReactPlayer playing={true} url={props.videourl} height={224} width={320}></ReactPlayer>: <img className="object-cover h-full w-full rounded-t-lg" src={props.imgurl} alt="image"></img>}
-    </div>
+    const [check, Setcheck] = useState(false)
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+    const handleMouseEnter = () => {
+        timerRef.current = setTimeout(() => {
+            Setcheck(true)
+        }, 250); 
+    }
+    const handleMouseLeave = () => {
+        if (timerRef.current) {
+            clearTimeout(timerRef.current);
+            timerRef.current = null;
+        }
+        Setcheck(false);
+    }
+
+    return (
+        <div 
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="w-80 h-56"
+        >
+            {check ? (
+                <ReactPlayer playing={true} url={props.videourl} height={224} width={320} />
+            ) : (
+                <img 
+                    className="object-cover h-full w-full rounded-t-lg" 
+                    src={props.imgurl} 
+                    alt="image"
+                />
+            )}
+        </div>
+    );
 }
