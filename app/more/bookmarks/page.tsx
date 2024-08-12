18e86d9b12f27b3646ef14aa3e48342739e01378
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-12 22:42:06 +0530

    fixed keys

--- app/more/bookmarks/page.tsx
+++ app/more/bookmarks/page.tsx
@@ -10,7 +10,7 @@ export default async function Watchlater(){
     return <div className="min-h-screen w-full flex flex-col justify-start bg-gray-100 items-center pt-20 md:pl-20">
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
         {data.map((announcement)=>{
-            return <AnnoucementCard key={announcement.id} id={announcement.annoucementid} discription={announcement.annoucement.discription} title={announcement.annoucement.title} userimg={announcement.annoucement.user.imgurl} likes={announcement.annoucement._count.annoucementlike} dislikes={announcement.annoucement._count.annoucementdislike} username={announcement.annoucement.user.name}  userdislike={announcement.user.annoucementdislike} userlike={announcement.user.annoucementlike} userbookmark={announcement.user.annoucementbookmark} >
+            return <AnnoucementCard  owerid={announcement.userid} key={announcement.id} id={announcement.annoucementid} discription={announcement.annoucement.discription} title={announcement.annoucement.title} userimg={announcement.annoucement.user.imgurl} likes={announcement.annoucement._count.annoucementlike} dislikes={announcement.annoucement._count.annoucementdislike} username={announcement.annoucement.user.name}  userdislike={announcement.user.annoucementdislike} userlike={announcement.user.annoucementlike} userbookmark={announcement.user.annoucementbookmark} >
             </AnnoucementCard>
         })}
         </div>
